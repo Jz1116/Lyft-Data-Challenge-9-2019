0e2a1549cd0b646ec13b20c2fc2433d6a6863aa3
zjiang330 <zjiang330@gatech.edu>
2019-10-10 20:20:03 -0400

    import

---
+++ data_manipulation.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from pandas.plotting import radviz
+import matploblib.pyplot as plt
+import numpy as np
+from sklearn.model_selection import train_test_split
+#import the model we are using 
