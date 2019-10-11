1429fd54bec43472e34d68b6b6b3c937fc89df7b
zjiang330 <zjiang330@gatech.edu>
2019-10-10 20:21:09 -0400

    import

--- data_manipulation.py
+++ data_manipulation.py
@@ -3,4 +3,9 @@ from pandas.plotting import radviz
 import matploblib.pyplot as plt
 import numpy as np
 from sklearn.model_selection import train_test_split
-#import the model we are using 
+#import the model we are using
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn import svm, preprocessing
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
